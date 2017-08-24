ee996aff791f1b5e421fa2c1405de0caa14d5881
Frank Boeddeker <frank.boeddeker@gmx.net>
2017-08-24 22:28:02 +0200

    Rearrange configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -3,8 +3,26 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
+let
+  elmPackages = with pkgs.elmPackages; [
+    elm
+    elm-format
+    elm-make
+  ]; 
 
-{
+  hsPackages = with pkgs.haskellPackages; [
+    alex
+    cabal2nix
+    cabal-install
+    ghc
+    ghcid
+    ghc-mod
+    happy
+    hlint
+    stack
+  ];
+
+in {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -14,14 +32,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.loader.grub.device = "/dev/sdb";
-
-  boot.initrd.luks.devices = [
-    { name = "rootfs";
-      device = "/dev/sdb2";
-      preLVM = true;
-      allowDiscards = true;  }
-  ];
+  boot.initrd.luks.devices = [ {
+    name = "root";
+    device = "/dev/sda2";
+    preLVM = true;
+  } ];
 
   nixpkgs.config.allowUnfree = true;
 
@@ -29,33 +44,66 @@
   networking.networkmanager.enable = true;
 
   hardware.pulseaudio.enable = true;
+  powerManagement.enable = true;
 
-  # Configure fonts
-  fonts = {
-    enableCoreFonts = true;
-    enableFontDir = true;
-    enableGhostscriptFonts = true;
-    fonts = with pkgs; [
-      anonymousPro
-      corefonts
-      dejavu_fonts
-      freefont_ttf
-      liberation_ttf
-      source-code-pro
-      terminus_font
-      ttf_bitstream_vera
-      ubuntu_font_family
-    ];
-  };
-
+  # Select internationalisation properties.
   i18n = {
-   consoleFont = "Lat2-Terminus16";
-   consoleKeyMap = "us";
-   defaultLocale = "en_US.UTF-8";
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
   };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    git
+    i3 i3status i3lock-fancy i3blocks-gaps feh
+    acpi
+    rofi
+    tmux
+    ranger
+    neomutt
+    which
+    htop
+
+    # To get make hopefully
+    glpk
+
+    # Browser
+    chromium
+    firefox
+
+    # PDF reader 
+    evince
+
+    # Notification
+    dunst
+
+    terminator
+    weechat
+    emacs
+    xlibs.xbacklight
+    xclip
+
+    # Nodejs
+    nodejs
+
+    # Scala
+    sbt
+    idea.idea-community
+    vscode
+
+  ] ++ hsPackages ++ elmPackages;
+
+  programs.zsh.enable = true;
 
   users.extraUsers.frank = {
-    name = "frank";
     group = "users";
     extraGroups = [
       "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal"
@@ -65,27 +113,26 @@
     shell = "/run/current-system/sw/bin/zsh";
   };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
-  powerManagement.enable = true;
-
- # Enable acpi
   services.acpid.enable = true;
-  services.acpid.lidEventCommands = ''
-    LID_STATE=/proc/acpi/button/lid/LID0/state
-    if [ $(/run/current-system/sw/bin/awk '{print $2}' $LID_STATE) = 'closed' ]; then
-      systemctl suspend
-    fi
-  '';
-
-
-  # Services
   services.dbus.enable = true;
   services.logind.extraConfig = "HandleLidSwitch=suspend";
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
     layout = "en";
+    windowManager.i3.package = pkgs.i3-gaps;
     windowManager.i3.enable = true;
     windowManager.default = "i3";
     desktopManager.xterm.enable = false;
@@ -96,68 +143,21 @@
       slim.defaultUser = "frank";
     };
 
-    multitouch = {
-      enable = true;
-      invertScroll = true;
-      ignorePalm = true;
-    };
+    synaptics.enable = false;
+    libinput.enable = true;
 
-    synaptics = {
-      enable = true;
-      twoFingerScroll = true;
-    };
+    config = ''
+      Section "InputClass"
+        Identifier     "Enable libinput for TrackPoint"
+        MatchIsPointer "on"
+        Driver         "libinput"
+        Option         "ScrollMethod" "button"
+        Option         "ScrollButton" "8"
+      EndSection
+    '';
   };
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  # nmtui for wifi setup
-  environment.systemPackages = with pkgs; [
-    i3
-    i3status
-    i3lock-fancy
-    acpi
-    feh
-    rofi
-    tmux
-    ranger
-    alpine
-    which
-    htop
-    unzip
-    chromium
-    spotify
-    wget
-    vim
-    terminator
-    git
-    slack
-
-    # Scala
-    sbt
-    idea.idea-community
-  ];
-
-
-  programs.zsh.enable = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
-
   # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "16.09";
+  system.stateVersion = "17.03";
 
 }
