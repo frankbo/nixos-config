32a3e5f570848957f2f2b4ec17a1ba7d387e7a01
Frank Böddeker <frank.boeddeker@holidaycheck.com>
2018-06-25 22:08:22 +0200

    Extract some config into common.nix

---
+++ nixos/common.nix
@@ -0,0 +1,128 @@
+{ config, pkgs, ... }:
+
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
+
+  # Configure fonts
+  fonts = {
+    enableCoreFonts = true;
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      fira-mono
+      font-awesome-ttf
+      anonymousPro
+      corefonts
+      dejavu_fonts
+      freefont_ttf
+      liberation_ttf
+      source-code-pro
+      terminus_font
+      ttf_bitstream_vera
+      ubuntu_font_family
+    ];
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    git
+    i3 i3status i3lock-fancy i3blocks-gaps feh
+    acpi
+    rofi
+    tmux
+    ranger
+    vscode
+
+    # Email
+    neomutt
+    thunderbird
+
+    which
+    htop
+
+    # Browser
+    chromium
+    firefox
+
+    # PDF reader 
+    evince
+
+    # Notification
+    dunst
+
+    # Automatically detect external screens
+    grobi
+
+    terminator
+    weechat
+    emacs
+    xlibs.xbacklight
+    xclip
+
+    # Nodejs
+    nodejs
+
+    # Scala
+    sbt
+  ];
+
+  programs.zsh.enable = true;
+
+  users.extraUsers.frank = {
+    group = "users";
+    extraGroups = [
+      "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" "docker"
+    ];
+    createHome = true;
+    home = "/home/frank";
+    shell = "/run/current-system/sw/bin/zsh";
+  };
+
+  services = {
+    acpid.enable = true;
+    logind.lidSwitch = "suspend";
+    xserver = {
+      enable = true;
+      layout = "us";
+      windowManager = {
+        i3.enable = true;
+        i3.package = pkgs.i3-gaps;
+        default = "i3";
+      };
+      desktopManager = {
+          xterm.enable = false;
+          default = "none";
+      };
+
+      displayManager = {
+        slim.enable = true;
+        slim.defaultUser = "frank";
+          sessionCommands = ''
+            ${pkgs.xlibs.xset}/bin/xset r rate 200 25  # set the keyboard repeat rate
+          '';
+      };
+    };
+  };
+}
---
+++ nixos/mbp-configuration.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      ./common.nix
+      ./umlauts.nix
+    ];
+
+  boot.initrd.luks.devices = [{
+    name = "root";
+    device = "/dev/disk/by-uuid/d44ab239-5e04-42a2-8c80-2ed0c07cacfc";
+    preLVM = true;
+  }];
+
+  environment.systemPackages = with pkgs; [
+    slack
+    jetbrains.idea-ultimate
+    openfortivpn
+    ngrok
+  ];
+  
+  services.xserver = {
+    resolutions = [ { x = 1680; y = 1050;} ];
+
+    multitouch = {
+      enable = true;
+      invertScroll = true;
+    };
+
+    synaptics = {
+      fingersMap = [ 0 0 0 ];
+      buttonsMap = [ 1 3 2 ];
+      additionalOptions = ''
+        Option "VertScrollDelta" "-100"
+        Option "HorizScrollDelta" "-100"
+      '';
+      enable = true;
+      tapButtons = false;
+      twoFingerScroll = true;
+    };
+  };
+
+  hardware = {
+    bluetooth.enable = false;
+    facetimehd.enable = true;
+  };
+
+  virtualisation.docker.enable = true;
+
+  
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  system.stateVersion = "18.03";
+}
