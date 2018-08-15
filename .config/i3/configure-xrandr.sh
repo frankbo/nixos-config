d3665d2b8544108ff6fb581f15dcd659f8244955
frankbo <frank.boeddeker@gmx.net>
2018-08-15 19:50:12 +0200

    Add screen detection script

--- .config/i3/configure-xrandr.sh
+++
@@ -1,9 +0,0 @@
-#!/run/current-system/sw/bin/bash -e
-
-PRIMARY="LVDS1"
-HDMI1="HDMI1"
-
-if (xrandr | grep "$HDMI1 connected"); then
-        xrandr --output $HDMI1 --primary --auto --left-of $PRIMARY
-        echo "External screen set as primary"
-fi
