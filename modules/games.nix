0673097b60f8bd13ad5f01de7ebbfb2c20d704a9
fboeddeker <frank.boeddeker@holidaycheck.com>
2019-05-07 16:38:05 +0200

    Fix steam setup

--- modules/games.nix
+++ modules/games.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    steam
+  users.users.frank.packages = [
+    pkgs.steam
   ];
+
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
 }
